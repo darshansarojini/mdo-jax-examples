6ecb4181c9136336386b354530e03bfc8bb1634e
Taran-Bhandari <160046604+Taran-Bhandari@users.noreply.github.com>
2025-09-21 17:57:47 -0400

    Create c172_propulsion.py

---
+++ casadi/c172/c172_propulsion.py
@@ -0,0 +1,12 @@
+import numpy as np
+from casadi import MX, vertcat, cross
+
+def propulsion(omega, vtas, ref_pt, thrust_origin, rho, prop_radius):
+    omega_RAD = (omega * 2 * np.pi) / 60.0
+    J = (np.pi * vtas) / (omega_RAD * prop_radius)
+    Ct = -0.1692121 * J ** 2 + 0.03545196 * J + 0.10446359
+    T = (2 / np.pi) ** 2 * rho * (omega_RAD * prop_radius) ** 2 * Ct
+    F_p = vertcat(T, 0, 0)
+    offset = ref_pt - thrust_origin
+    M_p = cross(offset, F_p)
+    return Ct, T, F_p, M_p
